328c4785b808beab8e696007d35bc4cf74336052
Kay Schäfer <148460726+Neuroklast@users.noreply.github.com>
2026-02-06 18:13:43 +0000

    Edited Spark

--- api/itunes.js
+++ api/itunes.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
-  const { term } = req.query; 
   
-  if (!term) {
+  
+
     return res.status(400).json({ error: 'Search term is required' });
   }
 
@@ -14,9 +14,9 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-}
+
+
+
+
+
