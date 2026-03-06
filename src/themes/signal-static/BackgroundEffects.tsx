f253d29552d8f14219c3fa75ba6058864a8c699f
Kay Schäfer <148460726+Neuroklast@users.noreply.github.com>
2026-03-06 18:26:14 +0100

    Add files via upload

---
+++ src/themes/signal-static/BackgroundEffects.tsx
@@ -0,0 +1,17 @@
+export default function BackgroundEffects() {
+  return (
+    <div className="fixed inset-0 pointer-events-none z-0">
+      <div className="signal-static-bg-noise"></div>
+      
+      <div className="signal-static-bg-scanlines"></div>
+      
+      <div className="signal-static-vignette"></div>
+      
+      <div className="signal-static-grid"></div>
+      
+      <div className="signal-static-signal-interference"></div>
+      
+      <div className="signal-static-chromatic-aberration"></div>
+    </div>
+  )
+}
